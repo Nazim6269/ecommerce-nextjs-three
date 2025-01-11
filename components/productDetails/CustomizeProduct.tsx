09cbfcefd862f2bf3cf4bf332f72c824e3671a75
Nazim Uddin <nazimuddin102001@gmail.com>
2025-01-11 19:07:56 +0600

    implemented state change of variants

--- components/productDetails/CustomizeProduct.tsx
+++ components/productDetails/CustomizeProduct.tsx
@@ -6,44 +6,42 @@ import { ProductVariantType } from "../product/ProductCard";
 const CustomizeProduct: React.FC<{ variants: ProductVariantType[] }> = ({
   variants,
 }) => {
-  const [selected, setSelected] = useState("");
+  const [selected, setSelected] = useState<{ [key: string]: string }>({});
 
-  console.log(variants, "variants");
-
-  const handleClick = () => {
-    console.log("object");
+  const handleClick = (optionName: string, choiceDesc: string) => {
+    setSelected((prev) => {
+      const updated = { ...prev, [optionName]: choiceDesc };
+      return updated;
+    });
   };
 
-  //size array
-  let sizeArray: string[] = [];
-  variants.reduce((acc, cur) => {
-    const size = cur.choices.Size;
-    if (!sizeArray.includes(size)) {
-      sizeArray.push(size);
-    }
-    return acc;
-  }, []);
+  const variantInStock = () => {};
 
-  //color array
+  // Generate size and color arrays
+  let sizeArray: string[] = [];
   let colorArray: string[] = [];
-  variants.reduce((acc, cur) => {
-    const color = cur.choices.Color;
-    if (!colorArray.includes(color)) {
-      colorArray.push(color);
-    }
-    return acc;
-  }, []);
+
+  variants.forEach((variant) => {
+    const size = variant.choices.Size;
+    const color = variant.choices.Color;
+
+    if (!sizeArray.includes(size)) sizeArray.push(size);
+    if (!colorArray.includes(color)) colorArray.push(color);
+  });
 
   return (
     <div className="mt-2 select-none">
       <div className="capitalize font-semibold my-3">Size</div>
       <ul className="flex gap-2">
         {sizeArray?.map((item) => {
+          const isSelected = selected.size === item;
           return (
             <li
-              onClick={handleClick}
+              onClick={() => handleClick("size", item)}
               key={item}
-              className="bg-blue-600 text-white flex items-center justify-center h-8 rounded-md capitalize px-2"
+              className={`${
+                isSelected ? "bg-pink-500" : "bg-blue-600"
+              } text-white flex items-center justify-center h-8 rounded-md capitalize px-2`}
             >
               {item}
             </li>
@@ -52,12 +50,21 @@ const CustomizeProduct: React.FC<{ variants: ProductVariantType[] }> = ({
       </ul>
       <div className="capitalize font-semibold my-3">color</div>
       <ul className="flex gap-2 ">
-        {colorArray.map((color) => (
-          <li
-            style={{ backgroundColor: color }}
-            className="w-9 h-9 rounded-full border"
-          ></li>
-        ))}
+        {colorArray.map((color) => {
+          const isSelected = selected.color === color;
+          return (
+            <li
+              key={color}
+              onClick={() => handleClick("color", color)}
+              style={{ backgroundColor: color }}
+              className="w-9 h-9 rounded-full border flex justify-center items-center"
+            >
+              {isSelected && (
+                <div className="w-9 h-9 rounded-full ring-4 ring-offset-2 ring-offset-white"></div>
+              )}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
